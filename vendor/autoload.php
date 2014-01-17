71da3efffa2546a1ee4c7776b68acabaa90e3978
Claire Sassella <klariss.web@free.fr>
2014-01-17 17:36:57 +0100

    twig

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit301fb93a9fe2233f1d5ba939b491153a::getLoader();
+return ComposerAutoloaderInite069d88c20e2bdfa1ccab05521316167::getLoader();

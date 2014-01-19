4561096a8732665832916a475046b7c5292a78b1
Claire Sassella <klariss.web@free.fr>
2014-01-19 23:47:17 +0100

    debut tpl

---
+++ config.rb
@@ -0,0 +1,22 @@
+# Require any additional compass plugins here.
+require "zurb-foundation"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "web/css"
+sass_dir = "web/sass"
+images_dir = "web/images"
+javascripts_dir = "web/js"
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+environment = :dev
+
+if environment == :production
+  output_style = :compressed
+else
+  output_style = :expanded
+  sass_options = { :debug_info => true }
+end
--- web/index.php
+++ web/index.php
@@ -5,8 +5,13 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/', function() {
-    return 'Hello!';
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('layout.html.twig');
 });
 
 

e5ee46576e3efea48c7eb71068b5c9531ac043ad
Claire Sassella <klariss.web@free.fr>
2014-03-02 13:30:39 +0100

    ayurveda.js

---
+++ web/js/ayurveda.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+        $('#horizontalTab').responsiveTabs({
+            active: 0,
+            startCollapsed: 'accordion',
+            collapsible: 'accordion',
+            rotate: false,
+            animation: 'slide',
+            setHash: true
+        });
+        $("a.reveal-link").click(function(){$('#myModal').foundation('reveal', 'open');});
+        $("a.close-reveal-modal").click(function(){$('#myModal').foundation('reveal', 'close');});
+        $(".up").click(function(){
+            $("html").animate({ scrollTop: 0 }, "fast");
+            return false;
+        });
+    });
+    $(document).foundation({orbit: { }});
